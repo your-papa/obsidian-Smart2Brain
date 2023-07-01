6e556987f397a05c8349feabf2e133a653a6967c
Emilio Toledo <github@emiliotoledo.me>
2023-07-01 15:19:29 -0500

    Add: example view

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { Plugin } from "obsidian";
+import { ExampleView, VIEW_TYPE_EXAMPLE } from "./Views/ExampleView";
 import "./styles.css";
 
 interface ObsidianNoteConnectionsSettings {
@@ -12,12 +13,6 @@ const DEFAULT_SETTINGS: ObsidianNoteConnectionsSettings = {
 export default class ObsidianNoteConnections extends Plugin {
 	settings: ObsidianNoteConnectionsSettings;
 
-	async onload() {
-		await this.loadSettings();
-	}
-
-	onunload() {}
-
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
@@ -25,4 +20,34 @@ export default class ObsidianNoteConnections extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async onload() {
+		await this.loadSettings();
+		
+		this.registerView(
+      VIEW_TYPE_EXAMPLE,
+      (leaf) => new ExampleView(leaf)
+    );
+
+		this.addRibbonIcon("dice", "Activate view", () => {
+      this.activateView();
+    });
+	}
+
+	onunload() {
+		console.log("unloading plugin")
+	}
+
+	async activateView() {
+    this.app.workspace.detachLeavesOfType(VIEW_TYPE_EXAMPLE);
+
+    await this.app.workspace.getRightLeaf(false).setViewState({
+      type: VIEW_TYPE_EXAMPLE,
+      active: true,
+    });
+
+    this.app.workspace.revealLeaf(
+      this.app.workspace.getLeavesOfType(VIEW_TYPE_EXAMPLE)[0]
+    );
+  }
 }
