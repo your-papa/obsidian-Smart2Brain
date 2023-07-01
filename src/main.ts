7d081c1c69bd10c1acbc4732e2b25470d877da85
Emilio Toledo <github@emiliotoledo.me>
2023-07-01 13:45:59 -0500

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { Plugin } from "obsidian";
+
+interface ObsidianNoteConnectionsSettings {
+	mySetting: string;
+}
+
+const DEFAULT_SETTINGS: ObsidianNoteConnectionsSettings = {
+	mySetting: "default",
+};
+
+export default class ObsidianNoteConnections extends Plugin {
+	settings: ObsidianNoteConnectionsSettings;
+
+	async onload() {
+		await this.loadSettings();
+	}
+
+	onunload() {}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
---
+++ vite.config.ts
@@ -0,0 +1,42 @@
+import { svelte, vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import builtins from "builtin-modules";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+	plugins: [svelte({ preprocess: vitePreprocess() })],
+	build: {
+		lib: {
+			entry: "src/main",
+			formats: ["cjs"],
+		},
+		rollupOptions: {
+			input: {
+				main: "src/main.ts",
+				styles: "src/styles.css"
+			},
+			output: {
+				entryFileNames: "main.js",
+				assetFileNames: "styles.css",
+			},
+			external: [
+				"obsidian",
+				"electron",
+				"@codemirror/autocomplete",
+				"@codemirror/collab",
+				"@codemirror/commands",
+				"@codemirror/language",
+				"@codemirror/lint",
+				"@codemirror/search",
+				"@codemirror/state",
+				"@codemirror/view",
+				"@lezer/common",
+				"@lezer/highlight",
+				"@lezer/lr",
+				...builtins,
+			],
+		},
+		outDir: ".",
+		assetsDir: ".",
+		emptyOutDir: false,
+	},
+});
