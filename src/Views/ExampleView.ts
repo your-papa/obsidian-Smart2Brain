e50a747a90bc5a2a3cd8afba2ef0079855de38a1
Emilio Toledo <github@emiliotoledo.me>
2023-07-01 15:20:47 -0500

    Add: example view

---
+++ src/Views/ExampleView.ts
@@ -0,0 +1,27 @@
+import { ItemView } from "obsidian";
+
+import Component from "../Components/Example.svelte";
+
+export const VIEW_TYPE_EXAMPLE = "example-view";
+
+export class ExampleView extends ItemView {
+  component: Component;
+
+  getViewType() {
+    return VIEW_TYPE_EXAMPLE;
+  }
+
+  getDisplayText() {
+    return "Example view";
+  }
+
+  async onOpen() {
+    this.component = new Component({
+      target: this.contentEl,
+    });
+  }
+
+  async onClose() {
+    this.component.$destroy();
+  }
+}
