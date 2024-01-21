247ae5ed6e00b007732ab687ab4870e4985cfbb8
nicobrauchtgit <nkhl.dev@pm.me>
2024-01-21 14:22:31 +0100

    remove: file select modal

--- src/components/Input.svelte
+++ src/components/Input.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import { Notice, setIcon } from 'obsidian';
     import type { KeyboardEventHandler } from 'svelte/elements';
-    import { FileSelectModal } from '../main';
     import { runSecondBrainFromChat } from '../runSecondBrain';
     import { nanoid } from 'nanoid';
     import { plugin, chatHistory, chatInput, isEditing, isEditingAssistantMessage } from '../store';
@@ -30,11 +29,8 @@
             return;
         }
         isProcessing = true;
-        //TODO das is kaka
-        //$chatInput = test.value;
 
         if ($chatInput.trim() !== '') {
-            // let message: Message = { role: 'user', content: $chatInput };
             let userQuery = $chatInput;
             $chatInput = '';
             await runSecondBrainFromChat($plugin.data.isUsingRag, userQuery);
@@ -45,12 +41,16 @@
     }
     function injectContext(event: KeyboardEvent): KeyboardEventHandler<HTMLInputElement> {
         if (event.key !== '[') return;
-        new FileSelectModal(app).open();
+        new Notice('Injecting context...');
     }
     function handleRAGToggle() {
         $plugin.data.isUsingRag = !$plugin.data.isUsingRag;
         $plugin.saveSettings();
-        new Notice($plugin.data.isUsingRag ? 'Now chatting with your Second Brain' : 'Now chatting with the LLM');
+        new Notice(
+            $plugin.data.isUsingRag
+                ? 'Now chatting with your Second Brain'
+                : 'Now chatting with the LLM'
+        );
     }
 
     function handleDelete() {
@@ -62,7 +62,9 @@
         $chatHistory = [];
         $chatHistory.push({
             role: 'Assistant',
-            content: $plugin.data.initialAssistantMessage.replace('Assistant\n', '').replace('\n- - - - -', ''),
+            content: $plugin.data.initialAssistantMessage
+                .replace('Assistant\n', '')
+                .replace('\n- - - - -', ''),
             id: nanoid(),
         });
         $plugin.chatView.requestSave();
@@ -95,7 +97,12 @@
     <p class="inline-block m-0">Connected to your Notes:</p>
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     <!-- svelte-ignore a11y-no-static-element-interactions -->
-    <span class="checkbox-container" class:is-enabled={$plugin.data.isUsingRag} on:click={handleRAGToggle}><input type="checkbox" tabindex="0" /> </span>
+    <span
+        class="checkbox-container"
+        class:is-enabled={$plugin.data.isUsingRag}
+        on:click={handleRAGToggle}
+        ><input type="checkbox" tabindex="0" />
+    </span>
 </div>
 <div class="w-full flex gap-3 items-center">
     <p class="inline-block m-0">Change Chatview</p>
@@ -137,5 +144,10 @@
         on:keydown={handelEnter}
         on:keyup={injectContext}
     />
-    <button aria-label="Ask the AI" type="submit" class="h-8 px-4 py-2 rounded-r-md hover:bg-primary transition duration-300 ease-in-out" use:icon={'send'} />
+    <button
+        aria-label="Ask the AI"
+        type="submit"
+        class="h-8 px-4 py-2 rounded-r-md hover:bg-primary transition duration-300 ease-in-out"
+        use:icon={'send'}
+    />
 </form>
