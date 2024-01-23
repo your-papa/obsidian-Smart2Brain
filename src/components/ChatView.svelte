5b9fb3bff6ac70e7311e486db1c9cf0b6e26e4d3
Leo310 <leodev310@gmail.com>
2024-01-24 00:40:55 +0100

    feat(chatview): amazing settings drawer

--- src/components/ChatView.svelte
+++ src/components/ChatView.svelte
@@ -1,15 +1,65 @@
 <script lang="ts">
+    import { setIcon } from 'obsidian';
     import InputComponent from './Input.svelte';
     import MessagesComponent from './Messages.svelte';
     import GptMessages from './GPTMessages.svelte';
     import { plugin } from '../store';
 
+    const icon = (node: HTMLElement, iconId: string) => {
+        setIcon(node, iconId);
+    };
+
     let textarea: HTMLTextAreaElement;
+
+    let isOpen = false;
+    let isOpening = false;
+
+    function handleChatToggel() {
+        $plugin.data.isChat = !$plugin.data.isChat;
+        $plugin.saveSettings();
+    }
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <div class="h-full flex flex-col --background-modifier-border">
+    <div
+        aria-label="Open Settings"
+        class={`text-[--text-normal] flex justify-center hover:text-[--text-accent-hover] ${isOpening || isOpen ? 'hidden' : ''}`}
+        use:icon={'chevron-down'}
+        on:click={() => {
+            isOpening = true;
+            setTimeout(() => (isOpen = true), 300);
+        }}
+    />
+
+    <div class={`relative ${isOpening ? 'drawer open' : 'drawer'}`}>
+        <div class="h-full flex flex-col justify-center items-center">
+            <div use:icon={'brain-circuit'} class="w-[--icon-xl] h-[--icon-xl] *:!w-[--icon-xl] *:!h-[--icon-xl]" />
+            <h1 class="text-[--text-normal] text-center">
+                {$plugin.data.isIncognitoMode ? $plugin.data.ollamaGenModel.model : $plugin.data.openAIGenModel.modelName}
+            </h1>
+            <div class="flex gap-3 items-center">
+                <p class="inline-block m-0">Change Chatview</p>
+                <!-- svelte-ignore a11y-click-events-have-key-events -->
+                <!-- svelte-ignore a11y-no-static-element-interactions -->
+                <span class="checkbox-container" class:is-enabled={$plugin.data.isChat} on:click={handleChatToggel}
+                    ><input type="checkbox" tabindex="0" />
+                </span>
+            </div>
+        </div>
+        <div
+            aria-label="Close Settings"
+            class={`absolute bottom-0 z-10 left-1/2 -translate-x-1/2 text-[--text-normal] hover:text-[--text-accent-hover] ${
+                isOpen || isOpening ? '' : 'hidden'
+            }`}
+            use:icon={'chevron-up'}
+            on:click={() => {
+                isOpening = false;
+                setTimeout(() => (isOpen = false), 300);
+            }}
+        />
+    </div>
     {#if $plugin.data.isChat}
         <MessagesComponent bind:textarea />
     {:else}
@@ -18,3 +68,16 @@
     <InputComponent bind:textarea />
     <span class="mb-3" />
 </div>
+
+<style>
+    .drawer {
+        min-height: 0;
+        height: 0;
+        overflow: hidden;
+        transition: min-height 0.3s ease-in-out;
+    }
+
+    .drawer.open {
+        min-height: 33%;
+    }
+</style>
--- src/components/Input.svelte
+++ src/components/Input.svelte
@@ -79,11 +79,6 @@
         if (textarea.scrollHeight == 42) textarea.style.height = '2rem';
         else textarea.style.height = textarea.scrollHeight + 'px';
     }
-
-    function handleChatToggel() {
-        $plugin.data.isChat = !$plugin.data.isChat;
-        $plugin.saveSettings();
-    }
 </script>
 
 <div class="w-full flex gap-3 items-center">
@@ -91,12 +86,6 @@
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     <!-- svelte-ignore a11y-no-static-element-interactions -->
     <span class="checkbox-container" class:is-enabled={$plugin.data.isUsingRag} on:click={handleRAGToggle}><input type="checkbox" tabindex="0" /> </span>
-</div>
-<div class="w-full flex gap-3 items-center">
-    <p class="inline-block m-0">Change Chatview</p>
-    <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <!-- svelte-ignore a11y-no-static-element-interactions -->
-    <span class="checkbox-container" class:is-enabled={$plugin.data.isChat} on:click={handleChatToggel}><input type="checkbox" tabindex="0" /> </span>
 </div>
 {#if $chatHistory.length > 1}
     <div class="w-full flex gap-3 items-center">
