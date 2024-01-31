ffe37b1bccc3e2d2c4d2cab574affc5ca621674d
nicobrauchtgit <nkhl.dev@pm.me>
2024-01-31 16:19:02 +0100

    feat: is ollama/openai running

---
+++ src/controller/Ollama.ts
@@ -0,0 +1,25 @@
+import { requestUrl } from 'obsidian';
+import { plugin } from '../globals/store';
+import { get } from 'svelte/store';
+
+export async function isOllamaRunning() {
+    try {
+        const response = await requestUrl(get(plugin).data.ollamaGenModel.baseUrl + '/api/tags');
+        return response.status === 200;
+    } catch (error) {
+        if (error.toString() === 'Error: net::ERR_CONNECTION_REFUSED') console.error('Ollama is not running', error);
+        else console.error(error);
+        return false;
+    }
+}
+
+export async function origingsNotSet() {
+    try {
+        const response = await fetch(get(plugin).data.ollamaGenModel.baseUrl + '/api/tags');
+        return response.status === 200;
+    } catch (error) {
+        if (error.toString() === 'Error: net::ERR_CONNECTION_REFUSED') console.error('Ollama is not running', error);
+        else console.error(error);
+        return false;
+    }
+}
