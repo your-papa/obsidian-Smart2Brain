7cfac8b8364ed672fab3c7b19017ef9802b4721b
Leo310 <leodev310@gmail.com>
2024-02-01 11:57:28 +0100

    refactor(settings-drawer): smoother animation

--- src/components/Chat/ChatView.svelte
+++ src/components/Chat/ChatView.svelte
@@ -12,7 +12,6 @@
     let textarea: HTMLTextAreaElement;
 
     let isOpen = true;
-    let isOpening = true;
 
     function handleChatToggel() {
         $plugin.data.isChat = !$plugin.data.isChat;
@@ -23,24 +22,15 @@
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <div class="h-full flex flex-col --background-modifier-border">
-    <div
-        aria-label="Open Settings"
-        class={`text-[--text-normal] flex justify-center hover:text-[--text-accent-hover] ${isOpening || isOpen ? 'hidden' : ''}`}
-        use:icon={'chevron-down'}
-        on:click={() => {
-            isOpening = true;
-            setTimeout(() => (isOpen = true), 300);
-        }}
-    />
-    <div class={`relative ${isOpening ? 'h-[33%] min-h-[33%]' : 'h-0 min-h-0'} overflow-hidden transition-all duration-300 ease-in-out`}>
+    <div class={`relative ${isOpen ? 'h-[33%] min-h-[33%]' : 'h-[--icon-m] min-h-[--icon-m]'} overflow-hidden transition-all duration-300 ease-in-out`}>
         <div class="h-full flex flex-col justify-center items-center">
             <div use:icon={'brain-circuit'} class="w-[--icon-xl] h-[--icon-xl] *:!w-[--icon-xl] *:!h-[--icon-xl]" />
             <h1 class="text-[--text-normal] text-center">
                 {$isIncognitoMode
                     ? $plugin.data.ollamaGenModel.model
                     : $plugin.data.openAIGenModel.openAIApiKey
-                      ? $plugin.data.openAIGenModel.modelName
-                      : 'Please setup in settings'}
+                    ? $plugin.data.openAIGenModel.modelName
+                    : 'Please setup in settings'}
             </h1>
             <div class="flex gap-3 items-center">
                 <p class="inline-block m-0">Change Chatview</p>
@@ -51,17 +41,16 @@
                 </span>
             </div>
         </div>
-        <div
-            aria-label="Close Settings"
-            class={`absolute bottom-0 z-10 left-1/2 -translate-x-1/2 text-[--text-normal] hover:text-[--text-accent-hover] ${
-                isOpen || isOpening ? '' : 'hidden'
-            }`}
-            use:icon={'chevron-up'}
-            on:click={() => {
-                isOpening = false;
-                setTimeout(() => (isOpen = false), 300);
-            }}
-        />
+        <div class="absolute bottom-0 z-10 flex justify-center w-full bg-[--background-secondary]">
+            <div
+                aria-label={`${isOpen ? 'Close' : 'Open'} quick Settings`}
+                class={`text-[--text-normal] hover:text-[--text-accent-hover] transition-transform duration-300 ${
+                    isOpen ? 'transform rotate-180' : 'transform rotate-0'
+                }`}
+                use:icon={'chevron-down'}
+                on:click={() => (isOpen = !isOpen)}
+            />
+        </div>
     </div>
     {#if $plugin.data.isChat}
         <MessagesComponent bind:textarea />
