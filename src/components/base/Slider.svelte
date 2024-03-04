549b5d1825630ec2dc27faa619487116497494fa
nicobrauchtgit <nkhl.dev@pm.me>
2024-03-04 21:40:34 +0100

    revert(slider.svelte) ig this was fine

--- src/components/base/Slider.svelte
+++ src/components/base/Slider.svelte
@@ -2,7 +2,7 @@
     import { crossfade } from 'svelte/transition';
 
     export let options: Array<string>;
-    export let selected: string;
+    export let selected: string = options[0];
     const [send, recieve] = crossfade({ duration: 500 });
 </script>
 
