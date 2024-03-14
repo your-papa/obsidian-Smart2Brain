97f20e72748887209f38708c27b95afe9f5e76a5
Leo310 <leodev310@gmail.com>
2024-03-14 13:00:12 +0100

    refactor(settings): now disabled functionality

--- src/components/Settings/SettingContainer.svelte
+++ src/components/Settings/SettingContainer.svelte
@@ -2,9 +2,10 @@
     export let name: string;
     export let isHeading: boolean = false;
     export let desc: string = '';
+    export let isDisabled: boolean = false;
 </script>
 
-<div class="setting-item {isHeading ? 'setting-item-heading' : ''}">
+<div class="setting-item {isHeading ? 'setting-item-heading' : ''} {isDisabled ? 'opacity-50 pointer-events-none' : ''}">
     <div class="setting-item-info">
         <div class="setting-item-name">{name}</div>
         <div class="setting-item-description">{desc}</div>
