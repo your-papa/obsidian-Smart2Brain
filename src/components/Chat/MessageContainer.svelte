974b605dba3dbedb8926fec5bbacce0913d25f5c
nicobrauchtgit <nkhl.dev@pm.me>
2024-03-25 18:25:52 +0100

    feat(chatview): chat colors

--- src/components/Chat/MessageContainer.svelte
+++ src/components/Chat/MessageContainer.svelte
@@ -9,31 +9,27 @@
     {#if $data.isChatComfy}
         <div class="my-4 mr-4 flex justify-end">
             <!-- svelte-ignore a11y-click-events-have-key-events -->
-            <div class="group max-w-[80%] [&_p]:mb-2 rounded-t-lg rounded-bl-lg pb-1 px-4" style="background-color: var(--text-selection);">
+            <div class="group max-w-[80%] rounded-t-lg rounded-bl-lg bg-[--text-selection] px-4 pb-1 [&_p]:mb-2">
                 <slot />
             </div>
         </div>
     {:else}
         <div class="group border-x-0 border-b border-t-0 border-solid border-[--background-modifier-border] p-2 pr-4">
-            <div class="text-[--text-accent] mt-2 font-bold">{$t('chat.user')}</div>
+            <div class="mt-2 font-bold text-[--text-accent]">{$t('chat.user')}</div>
             <slot />
         </div>
     {/if}
 {:else if $data.isChatComfy}
-    <div
-        class="group ml-4 w-fit max-w-[80%] [&_p]:mb-2 rounded-t-lg rounded-br-lg pb-1 px-4 mt-4 pt-[1px] {$isChatInSidebar
-            ? 'bg-[--background-secondary]'
-            : 'bg-[--background-primary]'}"
-    >
+    <div class="group ml-4 mt-4 w-fit max-w-[80%] rounded-t-lg rounded-br-lg bg-[--background-primary-alt] px-4 pb-1 pt-[1px] [&_p]:mb-2">
         <slot />
     </div>
 {:else}
     <div
         class="group border-x-0 border-b border-t-0 border-solid border-[--background-modifier-border] p-2 pr-4 {$isChatInSidebar
-            ? 'bg-[--background-secondary]'
-            : 'bg-[--background-primary]'}"
+            ? 'bg-[--background-secondary-alt]'
+            : 'bg-[--background-primary-alt]'}"
     >
-        <div class="text-[--text-accent] mt-2 font-bold">{$t('chat.assistant')}</div>
+        <div class="mt-2 font-bold text-[--text-accent]">{$t('chat.assistant')}</div>
         <slot />
     </div>
 {/if}
