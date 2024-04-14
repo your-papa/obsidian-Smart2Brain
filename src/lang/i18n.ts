12733a7f7361db708d788e56c404bfc28b8c4221
Leo <48623649+Leo310@users.noreply.github.com>
2024-04-14 15:48:20 +0100

    feat(language): add spanish to i18n file (#95)

--- src/lang/i18n.ts
+++ src/lang/i18n.ts
@@ -1,9 +1,11 @@
 import { init, addMessages } from 'svelte-i18n';
 
 import en from './en.json';
+import es from './es.json';
 // import de from './de.json';
 
 addMessages('en', en);
+addMessages('es', es);
 // addMessages('de', de);
 
 init({
