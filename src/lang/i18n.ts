0d2c526e2d072f4fa4b957c8ce4765b2f1073cc9
Leo <48623649+Leo310@users.noreply.github.com>
2024-04-16 13:02:27 +0100

    feat(language): add turkish to i18n file (#98)

--- src/lang/i18n.ts
+++ src/lang/i18n.ts
@@ -2,10 +2,12 @@ import { init, addMessages } from 'svelte-i18n';
 
 import en from './en.json';
 import es from './es.json';
+import tr from './tr.json';
 // import de from './de.json';
 
 addMessages('en', en);
 addMessages('es', es);
+addMessages('tr', tr);
 // addMessages('de', de);
 
 init({
