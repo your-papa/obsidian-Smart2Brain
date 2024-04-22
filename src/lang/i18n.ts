a163169d8fdec64b4bf3e9d3187d17a9ab23ffb5
Probe <probe301@gmail.com>
2024-04-22 09:04:01 +0800

    feat: Add Simplified Chinese translation

--- src/lang/i18n.ts
+++ src/lang/i18n.ts
@@ -3,11 +3,13 @@ import { init, addMessages } from 'svelte-i18n';
 import en from './en.json';
 import es from './es.json';
 import tr from './tr.json';
+import zh from './zh.json';
 // import de from './de.json';
 
 addMessages('en', en);
 addMessages('es', es);
 addMessages('tr', tr);
+addMessages('zh', zh);
 // addMessages('de', de);
 
 init({
