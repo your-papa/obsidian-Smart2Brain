38ac1f457f702af36e142ecf2f146bf5e3334246
Maxence Maireaux <maxence@maireaux.fr>
2024-04-27 10:24:15 +0200

    feat: Add french language (#107)
    Co-authored-by: Leo <48623649+Leo310@users.noreply.github.com>

--- src/lang/i18n.ts
+++ src/lang/i18n.ts
@@ -3,11 +3,14 @@ import { init, addMessages } from 'svelte-i18n';
 import en from './en.json';
 import es from './es.json';
 import tr from './tr.json';
+import fr from './fr.json';
+
 // import de from './de.json';
 
 addMessages('en', en);
 addMessages('es', es);
 addMessages('tr', tr);
+addMessages('fr', fr);
 // addMessages('de', de);
 
 init({
