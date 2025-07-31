4a5189fedc786c6a637550b3fbbd8ddaff22eb27
nicobrauchtgit <nicobrauchtgit@proton.me>
2025-07-31 13:21:06 +0200

    toolbar

--- src/components/Chat/Chat.svelte
+++ src/components/Chat/Chat.svelte
@@ -3,19 +3,20 @@
     import Input from "./Input.svelte";
     import { getPlugin } from "../../lib/state.svelte";
     import { messenger } from "./chatState.svelte";
-    import { ChatView } from "../../views/Chat";
     import MessageContainer from "./MessageContainer.svelte";
+    import Toolbar from "./Toolbar.svelte";
     const plugin = getPlugin();
-    const iconStyle = "text-[--text-normal] hover:text-[--text-accent-hover]";
-
     const chat = messenger.createChat();
-    console.log(chat);
+    chat.title = "New Thread";
 </script>
 
 <QueryClientProvider client={plugin.queryClient}>
     <div
         class="--background-modifier-border rounded-t-md flex h-full flex-col gap-1 border border-solid border-[--background-modifier-border] bg-[--background-primary]"
     >
+        <div class="py-2 border border-solid border-t-0 border-x-0">
+            <Toolbar chatName={chat.title} />
+        </div>
         <div class="chat-window w-full flex-grow select-text overflow-y-scroll">
             <MessageContainer messages={chat.messages} />
         </div>
